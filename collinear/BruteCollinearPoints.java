57a53aaf6f75835ce637262f8b0a8c07712bdb2f
Aboco <anhhvu89@gmail.com>
2018-08-14 21:39:17 +0700

    100% collinear

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -13,22 +13,35 @@ public class BruteCollinearPoints {
         for(int i =0; i<numberOfPoints; i++){
             if(points[i]== null) throw new IllegalArgumentException();
         }
+
+        Point[] arrayPoints = new Point[numberOfPoints];
+
+        for(int i =0; i<numberOfPoints; i++){
+            arrayPoints[i] = points[i];
+        }
+
+        Arrays.sort(arrayPoints);
+
+        for(int i =0; i<numberOfPoints-1; i++){
+            if(arrayPoints[i].compareTo(arrayPoints[i+1])==0){
+                throw new IllegalArgumentException();
+            }
+        }
+
         numberOfSegments =0;
         segments = new LineSegment[0];
+
         if(numberOfPoints >=4){
             for(int i =0; i<numberOfPoints-3; i++){
                 for(int j =i+1; j<numberOfPoints-2; j++){
                     for(int k =j+1; k<numberOfPoints-1; k++){
                         for(int l =k+1; l<numberOfPoints; l++){
-                            double slope1 = points[i].slopeTo(points[j]);
-                            double slope2 = points[j].slopeTo(points[k]);
-                            double slope3 = points[k].slopeTo(points[l]);
-                            if(slope1 == Double.NEGATIVE_INFINITY || slope2==Double.NEGATIVE_INFINITY|| slope3== Double.NEGATIVE_INFINITY){
-                                throw new IllegalArgumentException();
-                            }
+                            double slope1 = arrayPoints[i].slopeTo(arrayPoints[j]);
+                            double slope2 = arrayPoints[j].slopeTo(arrayPoints[k]);
+                            double slope3 = arrayPoints[k].slopeTo(arrayPoints[l]);
                             if(slope1 == slope2 && slope2 == slope3){
-                                Point[] array4Point =  {points[i],points[j],points[k],points[l]};
-                                Arrays.sort(array4Point);
+                                Point[] array4Point =  {arrayPoints[i],arrayPoints[j],arrayPoints[k],arrayPoints[l]};
+                                // Arrays.sort(array4Point);
 
                                 addSegment(new LineSegment(array4Point[0],array4Point[3]));
                             }
@@ -43,13 +56,21 @@ public class BruteCollinearPoints {
     private  void addSegment(LineSegment segment){
 
         if(numberOfSegments == segments.length){
-            LineSegment[] copy = new LineSegment[segments.length +1];
+            LineSegment[] copy;
+            if(numberOfSegments==0){
+                copy = new LineSegment[segments.length+1];
+            }
+            else{
+                copy = new LineSegment[segments.length*2];
+            }
+
             for(int i =0; i< segments.length;i++){
                 copy[i] = segments[i];
             }
             segments = copy;
         }
         segments[numberOfSegments++] = segment;
+
     }
 
 
@@ -58,7 +79,12 @@ public class BruteCollinearPoints {
     }
 
     public LineSegment[] segments(){
-        return segments;
+        LineSegment[] copyOfSegments = new LineSegment[numberOfSegments];
+        for(int i = 0; i<numberOfSegments; i++ ){
+            copyOfSegments[i] = segments[i];
+        }
+
+        return copyOfSegments;
     }
 
 }
--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -6,91 +6,100 @@ public class FastCollinearPoints {
 
 
     public FastCollinearPoints(Point[] points){
-        if(points== null || points.length <4){
+        if(points== null){
             throw new IllegalArgumentException();
         }
         int numberOfPoints = points.length;
         numberOfSegments =0;
         segments = new LineSegment[0];
 
-        if(numberOfPoints >=4){
-            for(int i =0; i<numberOfPoints; i++){
-                if(points[i]== null) throw new IllegalArgumentException();
+
+        for(int i =0; i<numberOfPoints; i++){
+            if(points[i]== null) throw new IllegalArgumentException();
+        }
+
+        Point[] arrayPoints = new Point[numberOfPoints-1];
+
+
+        for(int i = 0; i <numberOfPoints; i++){
+            int index =0;
+            for(int j = 0; j<numberOfPoints; j++){
+                if(i!=j){
+                    double slope = points[i].slopeTo(points[j]);
+                    if(slope == Double.NEGATIVE_INFINITY){
+                        throw new IllegalArgumentException();
+                    }
+                    arrayPoints[index++]= points[j];
+                }
             }
+            Arrays.sort(arrayPoints,points[i].slopeOrder());
 
-            Point[] arrayPoints = new Point[numberOfPoints-1];
 
-            for(int i = 0; i <numberOfPoints; i++){
-                int index =0;
-                for(int j = 0; j<numberOfPoints; j++){
-                    if(i!=j){
-                        double slope = points[i].slopeTo(points[j]);
-                        if(slope == Double.NEGATIVE_INFINITY){
-                            throw new IllegalArgumentException();
-                        }
-                        arrayPoints[index++]= points[j];
+            Point firstPoint= points[i], lastPoint = points[i];
+            int countPoints=0;
+            double slope = Double.NEGATIVE_INFINITY;
+
+            for(int k=0; k < arrayPoints.length; k++) {
+                Point currentPoint = arrayPoints[k];
+                double currentSlope = points[i].slopeTo(currentPoint);
+
+                if(slope ==Double.NEGATIVE_INFINITY){
+                    slope = currentSlope;
+                    countPoints = 1;
+                    if(firstPoint.compareTo(currentPoint)>0){
+                        firstPoint = currentPoint;
+                    }
+                    else if(lastPoint.compareTo(currentPoint)<0){
+                        lastPoint = currentPoint;
                     }
                 }
-                Arrays.sort(arrayPoints,points[i].slopeOrder());
-
-
-                Point firstPoint= points[i], lastPoint = points[i];
-                int countPoints=0;
-                double slope = Double.NEGATIVE_INFINITY;
-
-                for(int k=0; k < arrayPoints.length; k++) {
-                    Point currentPoint = arrayPoints[k];
-                    double currentSlope = points[i].slopeTo(currentPoint);
-
-                    if(slope ==Double.NEGATIVE_INFINITY){
-                        slope = currentSlope;
-                        countPoints = 1;
-                        if(firstPoint.compareTo(currentPoint)>0){
-                            firstPoint = currentPoint;
-                        }
-                        else if(lastPoint.compareTo(currentPoint)<0){
-                            lastPoint = currentPoint;
-                        }
+                else if(slope == currentSlope){
+                    countPoints +=1;
+                    if(firstPoint.compareTo(currentPoint)>0){
+                        firstPoint = currentPoint;
                     }
-                    else if(slope == currentSlope){
-                        countPoints +=1;
-                        if(firstPoint.compareTo(currentPoint)>0){
-                            firstPoint = currentPoint;
-                        }
-                        else if(lastPoint.compareTo(currentPoint)<0){
-                            lastPoint = currentPoint;
-                        }
-                        if(k == arrayPoints.length-1 && countPoints>=3 && firstPoint.compareTo(points[i])==0){
-                            addSegment(new LineSegment(firstPoint,lastPoint ));
-                        }
+                    else if(lastPoint.compareTo(currentPoint)<0){
+                        lastPoint = currentPoint;
                     }
-                    else{
-                        if(countPoints>=3 && firstPoint.compareTo(points[i])==0){
-                            addSegment(new LineSegment(firstPoint,lastPoint ));
-                        }
-                        if(currentPoint.compareTo(points[i])>0){
-                            firstPoint = points[i];
-                            lastPoint = currentPoint;
-                        }
-                        else if(currentPoint.compareTo(points[i])<0){
-                            firstPoint = currentPoint;
-                            lastPoint = points[i];
-                        }
-                        slope = currentSlope;
-                        countPoints = 1;
+                    if(k == arrayPoints.length-1 && countPoints>=3 && firstPoint.compareTo(points[i])==0){
+                        addSegment(new LineSegment(firstPoint,lastPoint ));
                     }
-
                 }
+                else{
+                    if(countPoints>=3 && firstPoint.compareTo(points[i])==0){
+                        addSegment(new LineSegment(firstPoint,lastPoint ));
+                    }
+                    if(currentPoint.compareTo(points[i])>0){
+                        firstPoint = points[i];
+                        lastPoint = currentPoint;
+                    }
+                    else if(currentPoint.compareTo(points[i])<0){
+                        firstPoint = currentPoint;
+                        lastPoint = points[i];
+                    }
+                    slope = currentSlope;
+                    countPoints = 1;
+                }
+
             }
         }
+
+
+
 
     }
 
     private  void addSegment(LineSegment segment){
 
         if(numberOfSegments == segments.length){
+            LineSegment[] copy;
+            if(numberOfSegments==0){
+                copy = new LineSegment[segments.length+1];
+            }
+            else{
+                copy = new LineSegment[segments.length*2];
+            }
 
-            LineSegment[] copy = new LineSegment[segments.length +1];
             for(int i =0; i< segments.length;i++){
                 copy[i] = segments[i];
             }
@@ -105,6 +114,11 @@ public class FastCollinearPoints {
     }
 
     public LineSegment[] segments(){
-        return segments;
+        LineSegment[] copyOfSegments = new LineSegment[numberOfSegments];
+        for(int i = 0; i<numberOfSegments; i++ ){
+            copyOfSegments[i] = segments[i];
+        }
+
+        return copyOfSegments;
     }
 }
